baa47f9c65f10259c9dde7a1831dd3f7fd9ee9b6
bbarreto08 <brunocbarreto2012@gmail.com>
2023-09-17 11:44:33 -0300

    Learn how to use components and css modules

--- FundamentosReactJS/src/App.jsx
+++ FundamentosReactJS/src/App.jsx
@@ -1,17 +1,32 @@
 import { Post } from './Post';
+import { Header } from './components/Header.jsx';
+
+import styles from './App.module.css';
+import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="Bruno Barreto" 
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis error praesentium quaerat perferendis molestias sequi non enim adipisci provident quae illo, eaque accusamus, minus expedita excepturi quam? Iure, reiciendis velit?"
-      />
+      <Header></Header>
+
+      <div className={styles.wrapper}>
+
+        <Sidebar></Sidebar>
+
+        <main>
+          <Post 
+            author="Bruno Barreto" 
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis error praesentium quaerat perferendis molestias sequi non enim adipisci provident quae illo, eaque accusamus, minus expedita excepturi quam? Iure, reiciendis velit?"
+          />
       
-      <Post 
-        author="Jovem Dev" 
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis error praesentium quaerat perferendis molestias sequi non enim adipisci provident quae illo, eaque accusamus, minus expedita excepturi quam? Iure, reiciendis velit?"
-      />
+          <Post 
+            author="Jovem Dev" 
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis error praesentium quaerat perferendis molestias sequi non enim adipisci provident quae illo, eaque accusamus, minus expedita excepturi quam? Iure, reiciendis velit?"
+          />
+        </main>
+
+      </div>
 
     </div>
     
---
+++ FundamentosReactJS/src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            
+            <img
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80"
+
+
+            />
+            
+            <div className={styles.profile}>
+                <img
+                    className={styles.avatar} 
+                    src="https://avatars.githubusercontent.com/u/19316132?v=4"/>
+                <strong>Bruno Barreto</strong>
+                <span>Tech Lead</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+
+        </aside>
+    );
+}
